e3b9550cbe94612c958c76b257e3b3bd80f65ae0
hawn1753 <john175258@gmail.com>
2021-02-24 23:07:33 +0900

    Upload 박경환

---
+++ 3일차/11048 이동하기/BJ11048_hwan.py
@@ -0,0 +1,14 @@
+from sys import stdin
+
+N, M = map(int, stdin.readline().split())
+
+dp = [[0] * (M+1)]
+for i in range(N):
+    dp.append([0] + list(map(int, stdin.readline().split())))
+
+
+for i in range(1,N+1):
+    for j in range(1, M+1):
+        dp[i][j] = max(dp[i-1][j]+dp[i][j], dp[i][j-1]+dp[i][j])
+
+print(dp[N][M])
